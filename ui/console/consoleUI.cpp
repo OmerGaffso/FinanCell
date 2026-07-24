4d38387bfbbc7dc52c89de3c35899a823e534ccf
Omer <penso.omer@gmail.com>
2026-07-24 17:22:43 +0300

    Added display main menu body. Added read choice body.

--- ui/console/consoleUI.cpp
+++ ui/console/consoleUI.cpp
@@ -12,14 +12,14 @@ void consoleUI::run_app()
         switch (choice)
         {
             case 0: 
-                std::cout << "GoodBye!" << std::endl;
+                std::cout << "GoodBye!\n" << std::endl;
                 isRunning = false;
                 break;
             case 1:
-                std::cout << "Cannot create account yet." << std::endl;
+                std::cout << "Cannot create account yet.\n" << std::endl;
                 break;
             case 2:
-                std::cout << "Cannot create a cell yet." << std::endl;
+                std::cout << "Cannot create a cell yet.\n" << std::endl;
             
             default:
                 break;
@@ -29,13 +29,29 @@ void consoleUI::run_app()
 
 void consoleUI::display_main_menu() const
 {
-    
+    std::cout << "==============" << std::endl;
+    std::cout << "FinanCell App" << std::endl;
+    std::cout << "==============" << std::endl;
+    std::cout << "1. Create Account" << std::endl;
+    std::cout << "2. Create Cell" << std::endl;
+    std::cout << "0. Exit" << std::endl;
+    std::cout << "==============" << std::endl;
 }
 
 int consoleUI::read_choice() const
 {
-    static int i = 1;
-    if (i == 3)
-        i = 0;
-    return i++;
+    int choice;
+    std::cout << "Enter your choice: ";
+    std::cin >> choice;
+
+    // Check for invalid input
+    if (std::cin.fail())
+    {
+        std::cin.clear(); // Clear the error flag
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Discard invalid input
+        std::cout << "Invalid input. Please enter a number." << std::endl;
+        return -1; // Return an invalid choice
+    }
+
+    return choice;
 }
