b027e51dacc6a0fdfc8267a70f77b5f20c18406a
Omer <penso.omer@gmail.com>
2026-07-25 18:38:52 +0300

    Added fields to FinancialCell

--- src/domain/FinancialCell.cpp
+++ src/domain/FinancialCell.cpp
@@ -1,8 +1,10 @@
 #include "FinancialCell.h"
 
-FinancialCell::FinancialCell(uint64_t id, std::string name, uint64_t creatorUserId)
-    : m_cellId(id), m_cellName(name), m_creatorId(creatorUserId)
-    {}
+FinancialCell::FinancialCell(uint64_t id, std::string name, std::string description, std::string usesCurrency, uint64_t ownerId)
+    : m_cellId(id), m_cellName(name), m_cellDescription(description), m_usesCurrency(usesCurrency), m_ownerId(ownerId)
+{
+
+}
 
 FinancialCell::~FinancialCell()
 {
@@ -14,12 +16,22 @@ uint64_t FinancialCell::getCellId() const
     return m_cellId;
 }
 
-uint64_t FinancialCell::getCreatorId() const
+uint64_t FinancialCell::getOwnerId() const
 {
-    return m_creatorId;
+    return m_ownerId;
 }
 
 const std::string& FinancialCell::getCellName() const
 {
     return m_cellName;
+}
+
+const std::string& FinancialCell::getCellDescription() const
+{
+    return m_cellDescription;
+}
+
+const std::string& FinancialCell::getUsesCurrency() const
+{
+    return m_usesCurrency;
 }
--- src/domain/FinancialCell.h
+++ src/domain/FinancialCell.h
@@ -6,15 +6,19 @@
 class FinancialCell
 {
 public:
-    FinancialCell(uint64_t id, std::string name, uint64_t creatorUserId);
+    FinancialCell(uint64_t id, std::string name, std::string description, std::string usesCurrency, uint64_t ownerId);
     ~FinancialCell() = default;
 
     uint64_t getCellId() const;
-    uint64_t getCreatorId() const;
+    uint64_t getOwnerId() const;
     const std::string& getCellName() const;
+    const std::string& getCellDescription() const;
+    const std::string& getUsesCurrency() const;
 
 private:
-    uint64_t m_creatorId;
+    uint64_t m_ownerId;
     uint64_t m_cellId;
     std::string m_cellName;
+    std::string m_cellDescription;
+    std::string m_usesCurrency;
 }; 
